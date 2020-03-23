ef1d1dd5fec0aec7e10b032669cad1f570f33175
chtjava <Wangshuai@DESKTOP-UHJNA5O>
2020-03-23 20:04:35 +0800

    aa

---
+++ src/main/java/com/B.java
@@ -0,0 +1,9 @@
+package com;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("AAAAAAAAAAAA");
+		System.out.println("BBBBBBBBBBBB");
+		System.out.println("CCCCCCCCCCCC");
+	}
+}
